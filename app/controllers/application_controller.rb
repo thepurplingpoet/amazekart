7e8679d86f5c64098d3ae8dbdc0f582f7ee5796e
gagan <gagsh1812@gmail.com>
2020-02-20 14:47:55 +0530

    add static routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user! , except: [:home, :show]
+  before_action :authenticate_user! , except: [:home, :show, :contact, :about]
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :seller])
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :seller, :password, :current_password) }
+
+  end
 
 end
