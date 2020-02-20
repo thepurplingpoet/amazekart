582ce0ee7b00002947fafb06517c9e5b2f507bec
Gagan Sharma <44533738+thepurplingpoet@users.noreply.github.com>
2020-02-21 03:46:30 +0530

    add products to homepage

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,24 @@
 class ProductsController < ApplicationController
 
   def show
+    @product = Product.find(params[:id])
+  end
+  def new
+  end
+
+  def create
+    @product = Product.new(product_params)
+ 
+  @product.save
+  redirect_to @product
+  end
+
+  private
+  def product_params
+    params.require(:product).permit(:name, :description)
+  end
+
+  def show_by_seller(seller)
     
   end
 end
