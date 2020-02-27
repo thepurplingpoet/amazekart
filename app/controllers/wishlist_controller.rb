7376a36441d5c7b6695f2a13c13127fdf144bfe5
gagan <gagsh1812@gmail.com>
2020-02-27 13:43:45 +0530

    edit wishlist

--- app/controllers/wishlist_controller.rb
+++ app/controllers/wishlist_controller.rb
@@ -1,9 +1,11 @@
 class WishlistController < ApplicationController
   
   def index
-    @wishlist_products = Product.find_by_sql("SELECT * FROM products
-    JOIN wishlists ON products.id = wishlists.product_id
-    where wishlists.user_id = #{current_user.id}")
+    Product.uncached do
+      @wishlist_products = Product.find_by_sql("SELECT * FROM products
+      JOIN wishlists ON products.id = wishlists.product_id
+      where wishlists.user_id = #{current_user.id}")
+    end
   end
 
   def create
@@ -14,6 +16,14 @@ class WishlistController < ApplicationController
       
     ensure
       redirect_to Product.find(@wishlist.product_id)
+    end
+  end
+
+  def destroy
+    @wishlist = Wishlist.find(params[:id])
+    @wishlist.destroy
+    respond_to do |format|
+      format.html { redirect_to wishlist_index_path, notice: 'Wishlist item removed' }
     end
   end
 
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :order_item
   belongs_to :order
+  has_many :wishlists
   validates :name, presence: true, length: {maximum: 50}
   validates :description, presence: true, length: {minimum: 5}
   validates :stock, presence: true, numericality: {only_integer: true, greater_than: -1}
