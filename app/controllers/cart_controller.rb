3de766267736401cbd342083a32ccd1aee26da52
gagan <gagsh1812@gmail.com>
2020-02-27 14:39:34 +0530

    remove item from cart

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -4,13 +4,16 @@ class CartController < ApplicationController
     cart = JSON.parse(cookies[:cart])
   
     user_cart = cart[current_user.id.to_s]
+
     if user_cart.nil?
       cart[current_user.id.to_s] = {"products" => []}
     end
+
     products = cart[current_user.id.to_s]["products"]
     if products.nil?
       products = []
     end
+
     products = products << cart_params[:product_id].to_s
     cart[current_user.id.to_s] = {
       products: products
@@ -20,7 +23,20 @@ class CartController < ApplicationController
   end
 
   def cart_params
-    params.require(:cart).permit(:product_id, :user_id)
+  params.require(:cart).permit(:product_id, :user_id)
+  end
+
+  def remove
+    cart = JSON.parse(cookies[:cart])
+    
+    user_cart = cart[current_user.id.to_s]
+    products = cart[current_user.id.to_s]["products"]
+    products.delete(cart_params[:product_id].to_s)
+    cart[current_user.id.to_s] = {
+      products: products
+    }
+    cookies[:cart] = JSON.generate(cart)
+    redirect_to pages_cart_path(current_user.id), method: :get
   end
 
  
--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
   get 'pages/checkout_success'
   get 'pages/profile'
   get 'cart/add' => 'cart#add'
+  delete 'cart/remove'=>'cart#remove'
 end
